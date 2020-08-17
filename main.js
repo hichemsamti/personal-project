91e7e9774315a203e96eaacc0909e3ff027447fb
Your Name <samtihichem@gmail.com>
2020-08-18 01:59:31 +0200

    progressed

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
  	$(document).ready(function(){
-  	 $(".logo").animate({left: "600px"})
-}); 
- 	$(document).ready(function(){
- 	
- })
+  	 $("#logo").animate({left: "600px"})
+  	})
+ 
+ 
+
+  
+   $(document).ready(function(){
+  
+   $("#logo").mouseenter(function(){
+   	alert(" BE Careful it is Since 1920!!!!")
+   })
+})
+
+
+
